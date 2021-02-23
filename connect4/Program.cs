1b28adc3bffd8b9d4d4135f71069b3820e12461a
Kaldesyvon <martinnovysedlak99@gmail.com>
2021-02-23 18:48:58 +0100

    initial commit

---
+++ connect4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace connect4
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
